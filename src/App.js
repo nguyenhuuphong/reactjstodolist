dd7de528d298ebc89c31bcf23d0d5c224dfb2e0d
nguyenhuuphong <huyhuyjff@gmail.com>
2021-01-13 20:35:34 +0700

    edit todolist

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todos = [
+    'Go to market',
+    'Buy food',
+    'Make dinner'
+  ];
+    const list = todos.map( todo => {
+      return <li > {todo} </li>
+        }) ;  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
+      <ul className='todo'> {list} </ul>
+         
+          
+          
       </header>
     </div>
   );
